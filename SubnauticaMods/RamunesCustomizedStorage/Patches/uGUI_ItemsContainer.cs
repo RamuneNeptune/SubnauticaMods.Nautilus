68904165badcd1c11975448c649b4298e7a5f44d
ramennoodlesxv <94365980+RamuneNeptune@users.noreply.github.com>
2024-01-05 01:01:54 +1000

    Update uGUI_ItemsContainer.cs

--- SubnauticaMods/RamunesCustomizedStorage/Patches/uGUI_ItemsContainer.cs
+++ SubnauticaMods/RamunesCustomizedStorage/Patches/uGUI_ItemsContainer.cs
@@ -10,12 +10,10 @@ namespace Ramune.RamunesCustomizedStorage.Patches
         {
             float x = __instance.rectTransform.anchoredPosition.x;
 
-            __instance.rectTransform.anchoredPosition = new Vector2(x, 
+            __instance.rectTransform.anchoredPosition = new Vector2(Mathf.Sign(x) * (width == 8 ? 292f : 284f), 
                 (height == 9) ? -39f :        // if height is 9:       new Vector2(x, -39f)
                 (height == 10) ? -75f : -4f); // if height is 10:      new Vector2(x, -75f)
                                               // if height is neither: new Vector2(x, -4f)
-
-            __instance.rectTransform.anchoredPosition = new Vector2(Mathf.Sign(x) * (width == 8 ? 292f : 284f), __instance.rectTransform.anchoredPosition.y);
         }
     }
 }
